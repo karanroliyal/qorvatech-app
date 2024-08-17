c6fde2b147a93162f35f81bba904945185e198d7
Neetu <karanroliyal12@gmail.com>
2024-08-17 18:49:28 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -86,7 +86,7 @@ export default async function Home() {
         {/* Solution service qorvatech  */}
 
         {/* My Team Component  */}
-        {/* <MyTeam /> */}
+        <MyTeam />
         {/* My Team Component  */}
 
         {/* My projects Component  */}
